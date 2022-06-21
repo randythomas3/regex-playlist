26a97e47ba720bc461f24dbd6c010ff28105368c
randy <randy.thomas3.9393@gmail.com>
2022-06-21 01:46:15 +0100

    Add regular expression
    adding the RegExp in video #13 and unwrapping the =>
    arrow function.
    kept some comments about different ways of the validation function

--- validation.js
+++ validation.js
@@ -1,1 +1,31 @@
-// validation script here
+const inputs = document.querySelectorAll('input')
+const patterns = {
+    telephone:/^\d{11}$/
+};
+
+
+function validation(field,regex){
+    console.log(regex.test(field.value))
+}
+
+// inputs.forEach((item) => {
+//     item.addEventListener('keyup',(e)=>{
+//         //console.log(e.target.attributes.name.value)
+//       validation(e.target,patterns[e.target.attributes.name.value])
+//    //validation(e.target,patterns['telephone'])
+//       })
+// 
+// });
+// THIS IS JUST A CONCISED CODE FOR THE ONE BELOW
+
+inputs.forEach(itemLooped);
+function itemLooped(item2){
+    item2.addEventListener('keyup',Do)
+    function Do(argument) {
+        // console.log(argument.target.attributes.name.value)
+   validation(argument.target,patterns[argument.target.attributes.name.value])
+    }
+}
+
+
+
